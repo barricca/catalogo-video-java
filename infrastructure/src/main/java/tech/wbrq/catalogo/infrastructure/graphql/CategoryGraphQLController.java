e2cb7a2c02a5aa9dd242d9bf0c4602276c4f4c42
Williams Barriquero <williamsgomes45@gmail.com>
2025-06-08 08:05:45 -0300

    feat: add saveCategory mutation and CategoryDTO for GraphQL integration

--- infrastructure/src/main/java/tech/wbrq/catalogo/infrastructure/graphql/CategoryGraphQLController.java
+++ infrastructure/src/main/java/tech/wbrq/catalogo/infrastructure/graphql/CategoryGraphQLController.java
@@ -1,11 +1,15 @@
 package tech.wbrq.catalogo.infrastructure.graphql;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import tech.wbrq.catalogo.application.category.list.ListCategoryOutput;
 import tech.wbrq.catalogo.application.category.list.ListCategoryUseCase;
+import tech.wbrq.catalogo.application.category.save.SaveCategoryUseCase;
+import tech.wbrq.catalogo.domain.category.Category;
 import tech.wbrq.catalogo.domain.category.CategorySearchQuery;
+import tech.wbrq.catalogo.infrastructure.category.models.CategoryDTO;
 
 import java.util.List;
 import java.util.Objects;
@@ -14,9 +18,13 @@ import java.util.Objects;
 public class CategoryGraphQLController {
 
     private final ListCategoryUseCase listCategoryUseCase;
+    private final SaveCategoryUseCase saveCategoryUseCase;
 
-    public CategoryGraphQLController(final ListCategoryUseCase listCategoryUseCase) {
+    public CategoryGraphQLController(final ListCategoryUseCase listCategoryUseCase,
+                                     final SaveCategoryUseCase saveCategoryUseCase
+    ) {
         this.listCategoryUseCase = Objects.requireNonNull(listCategoryUseCase);
+        this.saveCategoryUseCase = Objects.requireNonNull(saveCategoryUseCase);
     }
 
     @QueryMapping
@@ -30,5 +38,10 @@ public class CategoryGraphQLController {
         final var aQuery = new CategorySearchQuery(page, perPage, search, sort, direction);
 
         return this.listCategoryUseCase.execute(aQuery).data();
+    }
+
+    @MutationMapping
+    public Category saveCategory(@Argument final CategoryDTO input) {
+        return this.saveCategoryUseCase.execute(input.toCategory());
     }
 }
